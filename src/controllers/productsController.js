5c03f1ecc72c49db449a6d8bc87b095c4e80669b
Amit bhandari <162410742+amit159357@users.noreply.github.com>
2024-09-18 20:03:33 +0530

    Update productsController.js

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -2,8 +2,7 @@ import { ProductModel } from "../models/products.js";
 
 const GetProductsController = async (req, res) => {
   try {
-    console.log("Inside");
-    await ProductModel.insertMany(obj);
+ 
     const titles = await ProductModel.find();
     res
       .status(200)
